554ac10ee7b4c2605c9dc007126e0d985cbaf726
Slava K <slava.kostromin.2000@mail.ru>
2022-11-12 14:06:19 +0700

    Z_68

---
+++ Z_68/Program.cs
@@ -0,0 +1,16 @@
+﻿int ResultNumbers(int m, int n)
+{
+if (m == 0)
+return n + 1;
+if (n == 0)
+return ResultNumbers(m - 1, 1) ;
+return ResultNumbers(m - 1, ResultNumbers (m, n- 1));
+}
+
+
+Console.Clear();
+Console.Write("Введите число 1-ое: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2-ое: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ResultNumbers(m, n));
